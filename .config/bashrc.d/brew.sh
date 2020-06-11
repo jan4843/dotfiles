cf8d7f2acdd02831e4b4cbd80ebb2b25feec136d
Jan Vitturi <vitturi.jan@gmail.com>
2020-06-11 17:37:02 +0200

    Add autocompletion to brew aliases

--- .config/bashrc.d/brew.sh
+++ .config/bashrc.d/brew.sh
@@ -5,6 +5,9 @@ fi
 alias cask='brew cask'
 alias zap='brew cask zap --force'
 
+complete -F _brew_cask cask
+complete -F _brew_cask_list zap
+
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_EMOJI=1
