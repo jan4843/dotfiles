2499ce2790211d9ee57bb1f60a4a9ca6314aab3d
Jan Vitturi <vitturi.jan@gmail.com>
2020-08-30 11:52:32 +0200

    Add directory track support to dotfiles manager

--- .config/bashrc.d/dotfiles.sh
+++ .config/bashrc.d/dotfiles.sh
@@ -1,4 +1,16 @@
-alias dotfiles='git --git-dir="$HOME/.local/src/dotfiles.git" --work-tree="$HOME"'
+export DOTFILES_GIT_DIR=~/.local/src/dotfiles.git
+
+_dotfiles_git() {
+	git --git-dir="$DOTFILES_GIT_DIR" --work-tree="$HOME" "$@"
+}
+
+dotfiles() {
+	while read -r file; do
+		(cd; _dotfiles_git add --intent-to-add "${file%.gitkeep}")
+	done < <(cd; _dotfiles_git ls-files | grep '/\.gitkeep$')
+
+	_dotfiles_git "$@"
+}
 
 if type __git_wrap__git_main &>/dev/null || __load_completion git &>/dev/null; then
 	complete -o bashdefault -o default -o nospace -F __git_wrap__git_main dotfiles
