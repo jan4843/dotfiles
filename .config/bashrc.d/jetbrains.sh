c0b5e3e92044596d487f74166b6abfeacb492d69
Jan Vitturi <vitturi.jan@gmail.com>
2020-08-30 12:58:42 +0200

    Add JetBrains bash aliases

---
+++ .config/bashrc.d/jetbrains.sh
@@ -0,0 +1,12 @@
+if [[ $OSTYPE != darwin* ]]; then
+	return
+fi
+
+while read -r path; do
+	: "${path#/Applications/}"
+	app=${_%.app*}
+	bin=${path##*/}
+
+	# shellcheck disable=SC2139
+	alias "${bin}=open -na \"${app}.app\" --args" 
+done < <(printf '%s\n' /Applications/*/Contents/jbr/../MacOS/*)
