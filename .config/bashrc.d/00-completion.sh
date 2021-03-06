74f3d871eb3f54bb81ec3737687c16ab9263998f
Jan Vitturi <vitturi.jan@gmail.com>
2021-03-07 00:17:30 +0100

    Simplify bash bashrc

--- .config/bashrc.d/00-completion.sh
+++
@@ -1,3 +0,0 @@
-for source in /usr/{local,}/share/bash-completion/bash_completion; do
-	[[ -e "$source" ]] && . "$source"
-done
--- .config/bashrc.d/bash.sh
+++ .config/bashrc.d/bash.sh
@@ -1,28 +1,14 @@
+test -e /usr/local/etc/profile.d/bash_completion.sh && source $_
+
 alias -- -='cd -'
 alias ..='cd ..; pwd'
 alias \?='echo $?'
 
-if [[ $TERM_PROGRAM == Apple_Terminal ]]; then
-	rm -rf ~/.bash_sessions
-
-	shell_session_delete_expired() { return 0; }
-	shell_session_history_allowed() { return 0; }
-	shell_session_history_check() { return 0; }
-	shell_session_history_enable() { return 0; }
-	shell_session_save() { return 0; }
-	shell_session_save_history() { return 0; }
-	shell_session_update() { return 0; }
-fi
-
-mkdir -p ~/.cache/bash
-
 HISTCONTROL=ignoreboth
-HISTFILE=~/.cache/bash/history
-HISTSIZE=1000
-HISTTIMEFORMAT='%F %T '
 
-if type __git_ps1 &>/dev/null; then
+if type __git_ps1 &> /dev/null; then
 	GIT_PS1_SHOWDIRTYSTATE=1
+	GIT_PS1_SHOWUNTRACKEDFILES=1
 	GIT_PS1_STATESEPARATOR=
 else
 	alias __git_ps1=':'
@@ -30,24 +16,6 @@ fi
 
 PS1='\h:\W$(__git_ps1 "(%s)") \$ '
 
-for prompt in PS{1,2,4}; do
-	declare "$prompt=\[\e[90m\]${!prompt}\[\e[0m\]"
-done
-
-term_launch=1
-ensure_newline() {
-	if [[ -n $term_launch ]]; then
-		unset term_launch
-		return
-	fi
-
-	local cursor
-	printf '\e[6n'
-	IFS=\; read -sdR -a cursor
-
-	if (( cursor[1] != 1 )); then
-		printf '\e[7m%%\e[27m\n'
-	fi
-}
-
-PROMPT_COMMAND="ensure_newline${PROMPT_COMMAND:+; $PROMPT_COMMAND}"
+for p in PS1 PS2 PS4; do
+	declare "$p=\[\e[90m\]${!p}\[\e[0m\]"
+done; unset p
