6d4c767baa3604f7aa6f21570f96415485a2c4e5
Jan Vitturi <vitturi.jan@gmail.com>
2021-07-25 23:49:10 +0200

    Ensure newline before bash prompt

--- .config/bashrc.d/bash.sh
+++ .config/bashrc.d/bash.sh
@@ -19,3 +19,20 @@ PS1='\h:\W$(__git_ps1 "(%s)") \$ '
 for p in PS1 PS2 PS4; do
 	declare "$p=\[\e[90m\]${!p}\[\e[0m\]"
 done; unset p
+
+term_launch=1
+ensure_newline() {
+	if [[ -n $term_launch ]]; then
+		unset term_launch
+		return
+	fi
+
+	local cursor
+	printf '\e[6n'
+	IFS=\; read -sdR -a cursor
+	if (( cursor[1] != 1 )); then
+		printf '\e[7m%%\e[27m\n'
+	fi
+}
+
+PROMPT_COMMAND="ensure_newline${PROMPT_COMMAND:+; $PROMPT_COMMAND}"
