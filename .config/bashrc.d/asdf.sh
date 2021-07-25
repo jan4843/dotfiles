bf5b2abc42ca9c906851c4582b2e9dc281c05cf1
Jan Vitturi <vitturi.jan@gmail.com>
2021-07-25 23:45:45 +0200

    Update asdf bashrc for git installations

--- .config/bashrc.d/asdf.sh
+++ .config/bashrc.d/asdf.sh
@@ -1,1 +1,6 @@
-test -e /usr/local/opt/asdf/asdf.sh && source $_
+test -e "${ASDF_DIR:-$HOME/.asdf}/asdf.sh" ||
+test -e /usr/local/opt/asdf/asdf.sh &&
+source "$_"
+
+test -e "${ASDF_DIR:-$HOME/.asdf}/completions/asdf.bash" &&
+source "$_"
