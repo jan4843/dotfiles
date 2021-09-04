4b5780a58404a2a5b9772f1634d0b4f6cd2589e6
Jan Vitturi <vitturi.jan@gmail.com>
2021-09-04 18:56:51 +0200

    Add Docker Desktop auto-start bashrc

---
+++ .config/bashrc.d/docker.sh
@@ -0,0 +1,14 @@
+[[ $OSTYPE == darwin* ]] || return
+
+docker() {
+    if [ -n "$DOCKER_HOST" ] || [ -e ~/Library/Containers/com.docker.docker/Data/tasks/docker ]; then
+        command docker "$@"
+        return
+    fi
+
+    open -a Docker
+    while ! command docker version > /dev/null 2>&1; do
+        sleep 1
+    done
+    command docker "$@"
+}
