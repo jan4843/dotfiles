321eacdb0fdebdafc8fb20985914fb2ef3a901ac
Jan Vitturi <vitturi.jan@gmail.com>
2022-04-18 12:35:07 +0200

    Add asdf as git submodule

--- .config/bashrc.d/asdf.sh
+++ .config/bashrc.d/asdf.sh
@@ -1,6 +1,2 @@
-test -e "${ASDF_DIR:-$HOME/.asdf}/asdf.sh" ||
-test -e /usr/local/opt/asdf/asdf.sh &&
-source "$_"
-
-test -e "${ASDF_DIR:-$HOME/.asdf}/completions/asdf.bash" &&
-source "$_"
+source ~/.local/src/asdf/asdf.sh
+source ~/.local/src/asdf/completions/asdf.bash
