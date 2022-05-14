841f81e1bd449808b840cb4a5f546c99a8bcd1cd
Jan Vitturi <vitturi.jan@gmail.com>
2022-05-14 14:55:37 +0200

    Remove Docker Desktop auto-start bashrc

--- .config/bashrc.d/docker.sh
+++ .config/bashrc.d/docker.sh
@@ -6,18 +6,3 @@ alias @='docker run --rm -it -v "$PWD:/host" -w /host'
 
 alias compose='docker compose'
 complete -F _docker_compose compose
-
-[[ $OSTYPE == darwin* ]] || return
-
-docker() {
-    if [ -n "$DOCKER_HOST" ] || [ -e ~/Library/Containers/com.docker.docker/Data/tasks/docker ]; then
-        command docker "$@"
-        return
-    fi
-
-    open -a Docker
-    while ! command docker version > /dev/null 2>&1; do
-        sleep 1
-    done
-    command docker "$@"
-}
