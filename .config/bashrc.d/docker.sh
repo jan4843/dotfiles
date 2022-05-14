fa8b3e187043acb7071b1790551304e2f8ecae08
Jan Vitturi <vitturi.jan@gmail.com>
2022-05-14 14:56:46 +0200

    Add heredocker bin

--- .config/bashrc.d/docker.sh
+++ .config/bashrc.d/docker.sh
@@ -2,7 +2,22 @@ command -v docker > /dev/null || return
 
 export DOCKER_CONFIG=~/.config/docker
 
-alias @='docker run --rm -it -v "$PWD:/host" -w /host'
+_heredocker() {
+	local containers
+	containers=$(
+		docker image ls --format "{{.Repository}}:{{.Tag}}" |
+		grep -v '<none>' |
+		sed 's/:latest$//'
+	)
+	mapfile -t COMPREPLY < <(
+		compgen -W "$containers" -- "${COMP_WORDS[COMP_CWORD]}"
+		compgen -d -- "${COMP_WORDS[COMP_CWORD]}"
+	)
+}
+
+alias @='heredocker'
+complete -F _heredocker heredocker
+complete -F _heredocker @
 
 alias compose='docker compose'
 complete -F _docker_compose compose
