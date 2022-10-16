82c4bd6fed166ec6c280d38b86f9a0666dfdaded
Jan Vitturi <vitturi.jan@gmail.com>
2022-10-16 11:49:04 +0200

    Support all URLs in git clone nav bashrc

--- .config/bashrc.d/nav.sh
+++ .config/bashrc.d/nav.sh
@@ -1,9 +1,13 @@
 nav() {
-	if [[ $2 == *"://"* ]]; then
-		dir="$1/$(basename "$2" .git)"
+	if [[ $2 == *":"* ]]; then
+		local dir
+		if [[ $# -gt 2 ]]; then
+			dir="$1/$3"
+		else
+			dir="$1/$(basename "$2" .git)"
+		fi
 		git clone "$2" "$dir" && cd "$dir"
 		rmdir "$dir" 2> /dev/null
-		unset dir
 	else
 		cd "$1/$2"
 	fi
@@ -17,22 +21,22 @@ _nav() {
 	compopt -o nospace
 }
 
-dev() { nav ~/Developer "$1"; }
+dev() { nav ~/Developer "$@"; }
 _dev() { _nav ~/Developer; }
 complete -F _dev dev
 
-doc() { nav ~/Documents "$1"; }
+doc() { nav ~/Documents "$@"; }
 _doc() { _nav ~/Documents; }
 complete -F _doc doc
 
-dt() { nav ~/Desktop "$1"; }
+dt() { nav ~/Desktop "$@"; }
 _dt() { _nav ~/Desktop; }
 complete -F _dt dt
 
-dl() { nav ~/Downloads "$1"; }
+dl() { nav ~/Downloads "$@"; }
 _dl() { _nav ~/Downloads; }
 complete -F _dl dl
 
-opt() { nav /opt "$1"; }
+opt() { nav /opt "$@"; }
 _opt() { _nav /opt; }
 complete -F _opt opt
