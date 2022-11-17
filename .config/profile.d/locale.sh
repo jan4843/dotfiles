a036e1a7b0c1bb7fe2886261d31c8a11eee1a6cb
Jan Vitturi <vitturi.jan@gmail.com>
2022-11-17 19:21:59 +0100

    Add locale profile

---
+++ .config/profile.d/locale.sh
@@ -0,0 +1,15 @@
+unset LANG
+unset LANGUAGE
+unset LC_ADDRESS
+unset LC_COLLATE
+unset LC_CTYPE
+unset LC_IDENTIFICATION
+unset LC_MEASUREMENT
+unset LC_MESSAGES
+unset LC_MONETARY
+unset LC_NAME
+unset LC_NUMERIC
+unset LC_PAPER
+unset LC_TELEPHONE
+unset LC_TIME
+export LC_ALL=C
