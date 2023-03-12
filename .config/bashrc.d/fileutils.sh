8e9f3a7c21e1f009b2195b8aa6cc288547655cd4
Jan Vitturi <vitturi.jan@gmail.com>
2023-03-12 12:31:43 +0100

    Add verbose flags to more fileutils aliases

--- .config/bashrc.d/fileutils.sh
+++ .config/bashrc.d/fileutils.sh
@@ -1,15 +1,16 @@
-alias chmod='chmod -vv'
-alias +x='chmod +x'
-alias -- -x='chmod -x'
-
 alias cp='cp -v'
-
+alias mv='mv -v'
+alias rm='rm -v'
 alias ln='ln -v'
+alias mkdir='mkdir -v'
+alias rmdir='rmdir -v'
+alias chmod='chmod -vv'
+alias chown='chown -vv'
+alias chgrp='chgrp -vv'
+
+alias -- -x='chmod -x'
+alias -- +x='chmod +x'
 
 alias ls='ls -AFhv --color=auto'
 [[ $OSTYPE == darwin* ]] && alias ls='ls -AFh -G'
 alias ll='ls -l'
-
-alias mv='mv -v'
-
-alias rm='rm -v'
