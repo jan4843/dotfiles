819833805c22a84dde9b5d3f52435f279598c9e9
Jan Vitturi <vitturi.jan@gmail.com>
2023-03-12 13:30:12 +0100

    Rename heredocker bin to @

--- .config/bashrc.d/docker.sh
+++ .config/bashrc.d/docker.sh
@@ -1,22 +1,19 @@
-command -v docker > /dev/null || return
+command -v docker >/dev/null || return
+
+alias compose='docker compose'
+_completion_loader docker-compose
+complete -F _docker_compose compose
 
 _heredocker() {
-	local containers
-	containers=$(
+	local images
+	images=$(
 		docker image ls --format "{{.Repository}}:{{.Tag}}" |
 		grep -v '<none>' |
 		sed 's/:latest$//'
 	)
 	mapfile -t COMPREPLY < <(
-		compgen -W "$containers" -- "${COMP_WORDS[COMP_CWORD]}"
+		compgen -W "$images" -- "${COMP_WORDS[COMP_CWORD]}"
 		compgen -d -- "${COMP_WORDS[COMP_CWORD]}"
 	)
 }
-
-alias @='heredocker'
-complete -F _heredocker heredocker
 complete -F _heredocker @
-
-alias compose='docker compose'
-_completion_loader docker-compose
-complete -F _docker_compose compose
