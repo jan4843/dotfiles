a217c4c771aa66761f4b7ebb0efe2a0e1f643c6a
Jan Vitturi <vitturi.jan@gmail.com>
2024-02-09 10:09:35 +0100

    Add kc function

--- .config/bashrc.d/kubectl.sh
+++ .config/bashrc.d/kubectl.sh
@@ -2,3 +2,16 @@ alias k='kubectl'
 
 _completion_loader kubectl
 complete -F __start_kubectl k
+
+kc() {
+	kubectl config set-context "${2:---current}" --namespace="$1"
+	test $# -gt 1 && kubectl config use-context "$2"
+}
+_kc() {
+	readarray -t COMPREPLY < <(
+		compgen -W "$(
+			kubectl get namespaces --output=jsonpath='{range .items[*].metadata.name}{@}{" "}{end}'
+		)" -- "${COMP_WORDS[COMP_CWORD]}"
+	)
+}
+complete -F _kc kc
